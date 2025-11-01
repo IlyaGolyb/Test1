c0bbff4dd0d89629db16b225a94a2b4894b2c9b3
IliyaGolub <golubilya34rus@gmail.com>
2025-11-01 09:35:22 +0300

    Подправка кода(исправление отображения картинки)

--- lib/main.dart
+++ lib/main.dart
@@ -66,9 +66,7 @@ class UserProfileScreen extends StatelessWidget {
                 children: [
                   const CircleAvatar(
                     radius: 60,
-                    backgroundImage: NetworkImage(
-                      'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80',
-                    ),
+                    backgroundImage: AssetImage('assets/123.png'),
                   ),
                   const SizedBox(height: 16),
                   Text(
